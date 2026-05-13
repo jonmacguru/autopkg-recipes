b1a579ae5a03d5d5f10055397f5ed83f35f555a7
jonmacguru <jon.taylor@bottlerocketstudios.com>
2026-05-13 12:35:01 -0400

    changes to downloader

--- ClaudeDesktop/Claude-LatestURL.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-REDIRECT_URL="https://claude.ai/redirect/claudedotcom.v1.0eb44107-cbdc-42bb-9747-a1bf0abd9d54/api/desktop/darwin/universal/dmg/latest/redirect"
-
-# Follow redirects but print the final URL
-FINAL_URL=$(curl -sSL -w "%{url_effective}" -o /dev/null "$REDIRECT_URL")
-echo "$FINAL_URL"
